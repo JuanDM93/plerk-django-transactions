9707b1cc9cda12bfe62e2bbd7989ef981dec442f
juandominguez <juan_dm93@hotmail.com>
2022-01-18 00:56:24 -0500

    basic auth

--- plerk/settings.py
+++ plerk/settings.py
@@ -119,6 +119,13 @@ USE_I18N = True
 USE_TZ = True
 
 
+# REST Framework
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
